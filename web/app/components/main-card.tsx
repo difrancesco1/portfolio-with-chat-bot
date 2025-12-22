a5d24ab6559faa09e6c5d0aeb1fd31459456cf16
Josh <joshiqua@gmail.com>
2025-12-22 14:12:38 -0800

    typewriter effect and open/close card

--- web/app/components/main-card.tsx
+++ web/app/components/main-card.tsx
@@ -1,10 +1,46 @@
 "use client";
+import { useState } from "react";
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
+import Typewriter from 'typewriter-effect';
 
 export default function MainCard() {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen((prev) => {
+      return !prev;
+    })
+  }
   return (
-    <div className="main-card-collapsed max-w-[28rem]">
-      <h1>Joshua DiFrancesco</h1>
-      <button className="menu-button-collapsed">Menu</button>
+    <div className={` max-w-[28rem] ${open ? 'main-card-open' : 'main-card-collapsed'}`}>
+      <div className='flex w-full items-center justify-between text-foreground'>
+        <div className="flex gap-1 items-center">
+          <span>Joshua DiFrancesco</span>
+          <span className='text-sm'> ~ %</span>
+          {!open && 
+          <Typewriter
+            options={{
+              strings: [
+                'ls projects',
+                'cat skills.txt',
+                './run typescript',
+                'python main.py',
+                'psql -d portfolio',
+                'curl api/contact',
+              ],
+              autoStart: true,
+              loop: true,
+              cursor: '▎',
+              cursorClassName: 'cursor-solid',
+            }}
+          />
+        }
+        </div>
+        <div className={`${open ? "menu-button-open" : "menu-button-collapsed"}`}>
+          { open ? <RemoveRoundedIcon onClick={toggleOpen}/> : <AddRoundedIcon onClick={toggleOpen}/> } 
+        </div>
+      </div>
     </div>
   );
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ import MainCard from "./components/main-card";
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen w-full">
         <MainCard />
       </div>
     </div>
